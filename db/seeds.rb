7d80242c007f7b7cbe5e56a3fc0937f4cbd14352
Charles Snollaerts <csnollaerts@gmail.com>
2023-03-08 14:56:48 -0300

    2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+categories = %w[chinese italian japanese french belgian]
+
+5.times do |i|
+  Restaurant.create(
+    name: "Restaurant #{i + 1}",
+    address: "#{i + 1} Main St",
+    phone_number: "123-456-789#{i + 1}",
+    category: categories.sample
+  )
+end
